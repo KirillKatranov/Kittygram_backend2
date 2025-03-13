d964aee8ca236c6f000c93800f3f13cc528c32ff
Кирилл Катранов <katranovrpk@gmail.com>
2025-03-13 15:24:53 +0300

    first commit

---
+++ cats/views.py
@@ -0,0 +1,23 @@
+from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+from rest_framework import permissions
+
+from .models import Achievement, Cat
+
+from .serializers import AchievementSerializer, CatSerializer
+
+
+class CatViewSet(viewsets.ModelViewSet):
+    queryset = Cat.objects.all()
+    serializer_class = CatSerializer
+    pagination_class = PageNumberPagination
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,) 
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user) 
+
+
+class AchievementViewSet(viewsets.ModelViewSet):
+    queryset = Achievement.objects.all()
+    serializer_class = AchievementSerializer
+    pagination_class = None
